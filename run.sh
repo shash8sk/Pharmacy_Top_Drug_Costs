40cec84933ffccbc9b507dfd2482ff01b1abec71
shash8sk <shash8sk@users.noreply.github.com>
2018-07-17 08:55:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+#
+python ./src/top_drug_costs.py ./input/itcont.txt ./output/top_cost_drug.txt
