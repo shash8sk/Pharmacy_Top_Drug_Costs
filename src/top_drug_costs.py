e8be256b3907d31767fe2ef74dd1127f61be34ca
shash8sk <shash8sk@users.noreply.github.com>
2018-07-17 10:13:48 -0400

    Update top_drug_costs.py

--- src/top_drug_costs.py
+++ src/top_drug_costs.py
@@ -8,8 +8,8 @@ import io
 import os
 import csv
 
-input_path = 'Input/itcont.txt'
-data_filename = os.path.join(os.path.join(os.path.dirname(__file__), '.'), input_path)
+
+data_filename = sys.argv[0]
 
 counter = 0
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','.']
@@ -44,8 +44,8 @@ with open(data_filename, buffering = 20000000) as infile:
 cost_sort = [(k, dict_cost[k]) for k in sorted(dict_cost, key=dict_cost.get, reverse=True)]
 #print(cost_sort)
 
-out_path = 'Output/top_cost_drug.txt'
-out_filename = os.path.join(os.path.join(os.path.dirname(__file__), '.'), out_path)
+
+out_filename = sys.argv[1]
 output = open(out_filename, 'w')
 output.write('drug_name,num_prescriber,total_cost\n')
 
